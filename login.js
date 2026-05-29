f441c97db3344f8ed49cec89a7d69f2336d0ae4b
Kelly <kellykaram06@gmail.com>
2026-05-29 21:11:01 +0300

    .

--- login.js
+++ login.js
@@ -75,3 +75,19 @@ function goToPlanting(){
 function goToMaintenance(){
     window.location.href="maintenance.html";
 }
+function addToCart(productId) {
+    fetch("add_to_cart.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "id=" + productId
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert("Product added to cart!");
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
