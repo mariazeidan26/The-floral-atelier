ceba6d201f239e1941bf7cfb1deaa604bdd3007f
joetrak <joetrak6@gmail.com>
2026-05-31 17:33:01 +0300

    modifi

--- connection.php
+++ connection.php
@@ -1,7 +1,10 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'floral_atelier');
+if (gethostname() === "fedora" || file_exists("/home/joetrak")) {
+    $conn = new mysqli('localhost', 'Floral_Atelier', 'MFA021', 'floral_atelier');
+} else {
+    $conn = new mysqli('localhost', 'root', '', 'floral_atelier');
+}
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
